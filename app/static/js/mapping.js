b6f123b74432ebafc20aedcef464412b9c0b5f59
Artem <fynjyxbrjd@gmail.com>
2022-03-20 22:30:02 +0700

    Some more polygon implementations

--- app/static/js/mapping.js
+++ app/static/js/mapping.js
@@ -1,35 +1,68 @@
-const raster = new ol.layer.Tile({
-  source: new ol.source.OSM(),
-});
+const finish_button = document.getElementById("complete");
+const area_span = document.getElementById("the_area");
 
 const source = new ol.source.Vector({wrapX: false});
 
-const vector = new ol.layer.Vector({
-  source: source,
+const raster = new ol.layer.Tile({
+    source: new ol.source.OSM(),
 });
+
+const vector = new ol.layer.Vector({
+    source: source,
+});
+
+let current_projection = raster.getSource().getProjection();
+let new_projection = new ol.proj.Projection({code: "EPSG:4326"});
 
 const map = new ol.Map({
-  layers: [raster, vector],
-  target: 'map',
-  view: new ol.View({
-    center: [-11000000, 4600000],
-    zoom: 4,
-  }),
+    layers: [raster, vector],
+    target: 'map',
+    view: new ol.View({
+        center: [-11000000, 4600000],
+        zoom: 4,
+    }),
 });
 
-const typeSelect = "Polygon";
-
-let draw; // global so we can remove it later
-function addInteraction() {
-    draw = new ol.interaction.Draw({
-      source: source,
-      type: typeSelect,
-    });
-    map.addInteraction(draw);
-}
+let draw = new ol.interaction.Draw({
+    source: source,
+    type: "Polygon",
+});
+map.addInteraction(draw);
 
 document.getElementById('undo').addEventListener('click', function () {
-  draw.removeLastPoint();
+    draw.removeLastPoint();
 });
 
-addInteraction();
+document.getElementById('clear').addEventListener('click', function () {
+    let features = vector.getSource().getFeatures();
+    features.forEach((feature) => {
+        vector.getSource().removeFeature(feature);
+    });
+    finish_button.classList.remove("btn-success");
+    finish_button.classList.add("btn-outline-success");
+    finish_button.disabled = true;
+
+    area_span.textContent = "-";
+});
+
+source.on('addfeature', function(evt) {
+    let this_feature = evt.feature;
+    let features = vector.getSource().getFeatures();
+
+    let coordinates = this_feature.getGeometry().getCoordinates();
+    let this_polygon = new ol.geom.Polygon(coordinates);
+    let area = Math.round(this_polygon.getArea() * 10) / 10;
+
+    console.log(this_polygon.transform(current_projection, new_projection).getCoordinates());
+
+    finish_button.classList.remove("btn-outline-success");
+    finish_button.classList.add("btn-success");
+    finish_button.disabled = false;
+    area_span.textContent = area.toString();
+
+    features.forEach((feature) => {
+        if (feature !== this_feature) {
+            vector.getSource().removeFeature(feature);
+        }
+    });
+});
