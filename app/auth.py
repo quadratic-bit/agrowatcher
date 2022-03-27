cc881488e6e16f9a461f925c54721da6d4ea5926
Artem <fynjyxbrjd@gmail.com>
2022-03-27 15:52:45 +0700

    Added user auth

--- app/auth.py
+++ app/auth.py
@@ -1,13 +1,41 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, Response
+from flask_login import login_user, current_user
+
+from app.models.database import db
+from app.models.users import RegisterForm, User, LoginForm
 
 bp_auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 
-@bp_auth.route("/login")
-def login() -> str:
-    return render_template("auth/login.html")
+@bp_auth.route("/login", methods=["GET", "POST"])
+def login() -> str | Response:
+    if current_user.is_authenticated:
+        return redirect("/dashboard")
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/dashboard")
+        return render_template("auth/login.html", form=form, message="Неверный логин или пароль")
+    return render_template("auth/login.html", form=form)
 
 
-@bp_auth.route("/signup")
-def signup() -> str:
-    return render_template("auth/signup.html")
+@bp_auth.route("/signup", methods=["GET", "POST"])
+def signup() -> str | Response:
+    if current_user.is_authenticated:
+        return redirect("/dashboard")
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template("auth/signup.html", form=form, message="Пароли не совпадают")
+        if User.query.filter_by(email=form.email.data).first():
+            return render_template("auth/signup.html", form=form,
+                                   message="Такой пользователь уже существует")
+        user = User(email=form.email.data, name=form.name.data)  # noqa
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user, remember=False)
+        return redirect("/dashboard")
+    return render_template("auth/signup.html", form=form)
--- app/models/users.py
+++ app/models/users.py
@@ -2,24 +2,31 @@ from datetime import datetime
 
 from flask_login import UserMixin
 from flask_wtf import FlaskForm
-from wtforms import EmailField, PasswordField, BooleanField, SubmitField
+from werkzeug.security import generate_password_hash, check_password_hash
+from wtforms import EmailField, PasswordField, BooleanField, SubmitField, StringField
 from wtforms.validators import DataRequired
 
 from app.models.database import db, lm
 
 
 class User(db.Model, UserMixin):
-    __tablename__ = "users"
+    __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
 
     email = db.Column(db.String, unique=True, nullable=False, index=True)
-    hashed_password = db.Column(db.String, nullable=False)
-
     name = db.Column(db.String, nullable=False)
     surname = db.Column(db.String, nullable=True)
 
-    created_date = db.Column(db.DateTime, default=datetime.now)
+    created_date = db.Column(db.DateTime, nullable=False, default=datetime.now)
+
+    hashed_password = db.Column(db.String, nullable=True)
+
+    def set_password(self, plain_password: str) -> None:
+        self.hashed_password = generate_password_hash(plain_password)
+
+    def check_password(self, plain_password: str) -> bool:
+        return check_password_hash(self.hashed_password, plain_password)
 
     def __repr__(self) -> str:
         return f"<User {self.name} ({self.email})>"
@@ -30,6 +37,14 @@ class LoginForm(FlaskForm):
     password = PasswordField("Пароль", validators=[DataRequired()])
     remember_me = BooleanField("Запомнить меня")
     submit = SubmitField("Войти")
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField("Почта", validators=[DataRequired()])
+    name = StringField("Имя пользователя", validators=[DataRequired()])
+    password = PasswordField("Пароль", validators=[DataRequired()])
+    password_again = PasswordField("Повторите пароль", validators=[DataRequired()])
+    submit = SubmitField("Зарегистрироваться")
 
 
 @lm.user_loader
